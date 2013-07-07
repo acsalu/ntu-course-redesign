f87f21d109cb2dba6678cc104964d3633db563a4
Acsa Lu <acsa0210@gmail.com>
2013-07-07 14:01:22 +0800

    search result ready

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
+
 $(function() {
 
     var search = function(keyword) {
         console.log("Search for " + keyword);
+        $.getJSON("ajax/result.json", function(data) {
+            $('#search-result').html("");
+            var courses = data["courses"];
+            $('#result-num').html(courses.length);
+            for (var i = 0; i < courses.length; ++i) {
+                $('#search-result').append("<div class='thumbnail course'>" +
+                        "<h4 class='course-name'>" + courses[i]["name"] + "</h4>" +
+                        "<p>" + courses[i]["instructor"] + 
+                        "<button class='btn btn-small btn-info pull-right'>加入課表</button></p></div>");
+            }
+        });
     }
 
     $('#search-btn').click(function() {
@@ -10,5 +22,9 @@ $(function() {
 
     $('#keyword-text').keypress(function(event) {
         if (event.which == 13) search($(this).val());
+    });
+
+    $(document).on("click", '.course', function(event) {
+        console.log("add course " + $(this).find('.course-name').html());
     });
 });
